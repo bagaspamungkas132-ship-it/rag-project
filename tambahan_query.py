49196eeb6f67350b373bec8c7d90c2da1416004e
bagaspamungkas132-ship-it <bagaspamungkas132@gmail.com>
2026-08-28 16:33:33 +0700

    Update tambahan_query.py

--- tambahan_query.py
+++ tambahan_query.py
@@ -1,1 +1,8 @@
 curl -v https://llmqwen14b.wssdx.apps.cml.ocbcnisp.com/v1/models
+
+import httpx
+client = OpenAI(
+    api_key=API_KEY,
+    base_url=BASE_URL,
+    http_client=httpx.Client(verify=False)  # matikan verifikasi SSL sementara utk tes
+)
