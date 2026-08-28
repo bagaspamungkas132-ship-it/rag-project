cf74edc89397744dcbab92b90b4a6a9194946c7e
bagaspamungkas132-ship-it <bagaspamungkas132@gmail.com>
2026-08-28 16:37:26 +0700

    Update tambahan_query.py

--- tambahan_query.py
+++ tambahan_query.py
@@ -6,3 +6,6 @@ client = OpenAI(
     base_url=BASE_URL,
     http_client=httpx.Client(verify=False)  # matikan verifikasi SSL sementara utk tes
 )
+
+export SSL_CERT_FILE=/etc/ssl/certs/ca-certificates.crt
+export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
